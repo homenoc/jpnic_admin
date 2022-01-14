67ee161d990c41bf67d1a86c1623596e739a6b3c
yoneyan <github@yoneyan.net>
2022-01-15 04:50:15 +0900

    [add] Added django-mathfilters plugin.

--- jpnic_gui/settings.py
+++ jpnic_gui/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'jpnic_gui',
     'jpnic_gui.result',
+    'mathfilters'
 ]
 
 MIDDLEWARE = [
--- jpnic_gui/views.py
+++ jpnic_gui/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.db.models import Q
 from django.shortcuts import render
 
 from jpnic_gui.form import SearchForm
@@ -9,6 +10,9 @@ def index(request):
     form = SearchForm(request.GET)
     result = form.get_queryset().order_by()
 
+    count_no_data = result.filter(Q(address='', address_en='')).count()
+    print(count_no_data)
+
     paginator = Paginator(result, int(request.GET.get("per_page", "30")))
     page = int(request.GET.get("page", "1"))
     try:
@@ -18,6 +22,7 @@ def index(request):
 
     context = {
         "jpnic_page": events_page,
+        "count_no_data": count_no_data,
         "search_form": form,
     }
     return render(request, 'jpnic_gui/index.html', context)
