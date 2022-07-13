0c70910bab733939405aa66742681fdaaeb00b53
yoneyan <contact@yoneyan.net>
2022-07-13 12:59:41 +0900

    [support] Supported mysql and containered.

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ pipenv run python3 manage.py migrate
 pipenv run python3 manage.py createdomain
 pipenv run python3 manage.py createsuperuser
 ```
+
+### build for lima
+
+```
+DJANGO_SETTINGS_MODULE=jpnic_gui.production_settings
+echo ${DJANGO_SETTINGS_MODULE}
+sudo docker-compose build
+sudo docker-compose up
+```
---
+++ jpnic_gui/production_settings.py
@@ -0,0 +1,28 @@
+from jpnic_gui.settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# # candidates value for X-Forwarded-Host header
+ALLOWED_HOSTS = []
+
+# django backend database config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': 'db',
+        'PORT': 3306,
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+    }
+}
+
+SITE_TITLE = 'JPNIC管理システム'
+SITE_HEADER = 'JPNIC管理システム'
+
+# CSRF Check
+# CSRF_TRUSTED_ORIGINS = ['http://*.localhost', 'https://*.127.0.0.1']
