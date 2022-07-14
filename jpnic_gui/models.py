045b3428a7e5f1731d474dabb729376f6dfa93e3
yoneyan <contact@yoneyan.net>
2022-07-15 01:38:22 +0900

    [remove] Removed unnecessary column.

--- jpnic_gui/models.py
+++ jpnic_gui/models.py
@@ -8,13 +8,10 @@ class JPNIC(models.Model):
     name = models.CharField("名前", unique=True, max_length=100)
     is_active = models.BooleanField("有効", blank=True)
     is_ipv6 = models.BooleanField("IPv6", blank=False)
-
     asn = models.IntegerField("ASN")
-    ca_path = models.CharField("中間CA証明書", max_length=200, default="")
-    cert_path = models.CharField("Cert", max_length=200, default="")
-    key_path = models.CharField("key", max_length=200, default="")
-    p12_path = models.CharField("P12", max_length=200, default="")
-    p12_pass = models.CharField("P12 Pass", max_length=200, default="")
+    ca = models.CharField("中間CA証明書", max_length=1000, default="")
+    p12_base64 = models.CharField("p12 base64", max_length=1000, default="")
+    p12_pass = models.CharField("p12 Pass", max_length=200, default="")
 
     def __str__(self):
         return self.name
