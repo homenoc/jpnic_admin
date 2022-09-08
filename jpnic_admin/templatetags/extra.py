bb2ca9701c92c583c662f74f2d3977fd70e4c772
yoneyan <contact@yoneyan.net>
2022-09-08 23:44:26 +0900

    [fix] Fixed timezone.

--- jpnic_admin/templatetags/extra.py
+++ jpnic_admin/templatetags/extra.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import template
 
 register = template.Library()
@@ -16,3 +18,13 @@ def to_int(value):
     if not value:
         return None
     return int(value)
+
+
+@register.simple_tag
+def to_jst(gmt):
+    jst = gmt + datetime.timedelta(hours=9)
+    print(gmt + datetime.timedelta(hours=9))
+    if not gmt:
+        return "取得失敗"
+    return jst.strftime("%Y/%m/%d %H:%M(JST)")
+
